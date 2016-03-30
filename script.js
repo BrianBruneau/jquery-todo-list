1b9efded533b11f3344859889cec9a4d02151055
BrianBruneau <Brian8ax@gmail.com>
2016-03-30 09:30:34 -0700

    Fixed a couple things. Cant submit empty form

--- script.js
+++ script.js
@@ -10,8 +10,14 @@ $('#list_form').submit(function(event) {
 
   $(this).children('input:text').each(function(index, elem) {
     var value = $(elem).val() // <-- makes variable for the value of input fields
+
+    if (!value) {
+    alert('Please enter text to add a list item');
+    return;
+    } else {
     $('.to_do_list').append('<li><label>' + value + '</label><button class ="del">Delete</button></li>'); // <-- appends input values to location specified
     $(elem).val(''); // <-- clears inputs
+    }
   });
 
   console.log(event);
